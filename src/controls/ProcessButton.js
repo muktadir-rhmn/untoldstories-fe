c436394d0e908159bcf6323ea1afd8044641ec25
muktadir <muktadir.rhmn@gmail.com>
2021-02-16 08:03:02 +0600

    Improved ProcessButton.js

--- src/controls/ProcessButton.js
+++ src/controls/ProcessButton.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import {Button, Spinner} from "react-bootstrap";
 
-class ProcessButton extends React.Component{
+/**
+ * Props:
+ *      - hide boolean: to control visibility
+ *      - disabled boolean: to control disability
+ *      - isProcessing boolean: to disable and show spinner while processing
+ *      - onClick function: click handler
+ */
+class ProcessButton extends React.Component {
 
     render() {
+        if (this.props.hide) return "";
+
         if (this.props.isProcessing) {
             return <Button variant="info" disabled><Spinner animation="border" size="sm"/>{this.props.children}</Button>;
         } else {
