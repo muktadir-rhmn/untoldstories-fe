3b9d94e5de6909756c95bd49eb30bde1d5208572
muktadir <muktadir.rhmn@gmail.com>
2021-02-17 10:26:29 +0600

    refactored

--- src/story/FacebookSharer.js
+++ src/story/FacebookSharer.js
@@ -7,10 +7,11 @@ class FacebookSharer extends React.Component {
         const URL = `${baseURL}${this.props.path}`;
 
         return (
-            <div className="fb-share-button" data-href={URL}
-                 data-layout="button_count" data-size="large"><a target="_blank"
-                                                                 href={`https://www.facebook.com/sharer/sharer.php?u=${URL}&amp;src=sdkpreparse`}
-                                                                 className="fb-xfbml-parse-ignore">Share</a></div>
+            <div className="fb-share-button"
+                 data-href={URL}
+                 data-layout="button_count">
+            </div>
+
         );
     }
 }
